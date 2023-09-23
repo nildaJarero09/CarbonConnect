c0f47a63094b8da1e3aff9f3ee16de331a0e7cb4
nildaJarero09 <99058821+nildaJarero09@users.noreply.github.com>
2023-09-23 14:40:42 -0500

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login Page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+      </main>
+    </div>
+  )
+}
